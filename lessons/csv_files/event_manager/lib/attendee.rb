cbe71fb0e37c28f477aa0a9fdf212e97176c0d56
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-23 12:15:52 -0700

    Begin #display

--- lessons/csv_files/event_manager/lib/attendee.rb
+++ lessons/csv_files/event_manager/lib/attendee.rb
@@ -1,14 +1,18 @@
 class Attendee
-  attr_reader :id, :first_name, :last_name, :zip_code
+  attr_reader :id, :first_name, :last_name, :zipcode, :name
 
   def initialize(id, first_name, last_name, zipcode)
     @id = id
     @first_name = first_name
     @last_name = last_name
-    @zipcode = zipcode
+    @zipcode = zipcode || "00000"
     @zipcode = @zipcode.rjust(5, "0")
     @zipcode = @zipcode.length > 5 ? @zipcode[0..4] : @zipcode
     @name = first_name + " " + last_name
+  end
+
+  def display
+    "#{@id}: #{@name}, #{@zipcode}"
   end
 
 end
