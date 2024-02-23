b756270d5952866a0f85ebf4e1fb818ea985682d
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-23 10:52:02 -0700

    Add refactored zipcode

--- lessons/csv_files/event_manager/lib/attendee.rb
+++ lessons/csv_files/event_manager/lib/attendee.rb
@@ -6,6 +6,9 @@ class Attendee
     @first_name = first_name
     @last_name = last_name
     @zipcode = zipcode
+    @zipcode = @zipcode.rjust(5, "0")
+    @zipcode = @zipcode.length > 5 ? @zipcode[0..4] : @zipcode
+    @name = first_name + " " + last_name
   end
 
 end
