7ae7590f52e07d8e5d3af25a05efe10426b71eb6
Aurora Sanderson <AuroraTuring@gmail.com>
2024-03-05 20:59:44 -0700

    Do work

--- lessons/enumerables/exercises/find.rb
+++ lessons/enumerables/exercises/find.rb
@@ -1,11 +1,18 @@
 # 1. Using #each, write the code to find the first sister over four letters in length.
-# 2. Once you get the method below to work using #each, refactor your code to use #find. 
+# 2. Once you get the method below to work using #each, refactor your code to use #find.
 
 def find_first_long_named_sister
     sisters = ["Rose", "Kathleen", "Eunice", "Patricia", "Jean"]
-  
-    ### YOUR CODE HERE
 
+    sisters.find do |sister|
+        sister.length > 4
+    end
+
+    # sisters.each do |sister|
+    #     if sister.length > 4
+    #         return sister
+    #     end
+    # end
     # should return "Kathleen"
 end
 
--- lessons/enumerables/exercises/map.rb
+++ lessons/enumerables/exercises/map.rb
@@ -1,16 +1,19 @@
 # The method below returns an array of the brothers names in all caps using the method #each.
-# # Your Task: Update this method to use #map. 
+# # Your Task: Update this method to use #map.
 
 def kennedy_brothers
     brothers = ["Robert", "Ted", "Joseph", "John"]
-  
-    caps_brothers = []
-  
-    brothers.each do |brother|
-      caps_brothers << brother.upcase
+
+    brothers.map do |brother|
+      brother.upcase
     end
-  
-    caps_brothers
+
+    # caps_brothers = []
+
+    # brothers.each do |brother|
+    #   caps_brothers << brother.upcase
+    # end
+    # caps_brothers
 end
 
 p kennedy_brothers
